ded005a096c31c3675128e8ab31a9c76a32b7458
vova <volodymyr.tymoshchuk@gmail.com>
2025-04-13 14:38:38 -0700

    Initial semi-working new NRF

--- src/Comms.py
+++ src/Comms.py
@@ -5,23 +5,43 @@ import logging
 import queue
 import Wireless
 import paho.mqtt.client as mqtt
+from paho.mqtt.subscribeoptions import SubscribeOptions
 from Config import MQTT_PREFIX_WEB, MQTT_PREFIX_JMRI
 
-packetLocoAuth = 'r'
-packetLocoNorm = 'n'
+# packetLocoAuth = 'r'
+# packetLocoNorm = 'n'
 packetThrAuth = 'q'
 packetThrSub = 's'
 packetThrNorm = 'p'
 
 
-commandThrottle  = 't'
-commandDirection = 'd'
-commandLight     = '@'
+# commandThrottle  = 't'
+# commandDirection = 'd'
+# commandLight     = '@'
+
+
+CMD_AUTH = 'A';
+CMD_HEARTBEAT = 'H';
+
+CMD_THROTTLE = 'T';
+CMD_DIRECTION = 'D';
+CMD_SET_FUNCTION = 'F';
+CMD_GET_FUNCTION = 'P';
+CMD_SET_VALUE = 'S';
+CMD_GET_VALUE = 'G';
+CMD_LIST_VALUE = 'L';
+
+def toInt(value):
+  try:
+    return int(value)
+  except:
+    return 0
 
 class Loco:
   def __init__(self, addr, version, fmt, name, fields):
-    self.cmd = 't'
-    self.value = 0.0
+    # self.cmd = 't'
+    # self.value = 0.0
+    self.payload = struct.pack('<BB', ord(CMD_THROTTLE), 0)
     self.version = version
     self.fmt = fmt
     self.addr = addr
@@ -31,24 +51,23 @@ class Loco:
     self.queue = queue.Queue()
     self.throttles = {}
 
-  def toFloat(self, value):
-    try:
-      return float(value)
-    except:
-      return 0.0
+  # def toFloat(self, value):
+  #   try:
+  #     return int(value)
+  #   except:
+  #     return 0.0
 
-  def push(self, cmd, value):
-    value = self.toFloat(value)
+  def push(self, payload):
     if self.queue.empty():
-      self.cmd, self.value = (cmd, value)
-    self.queue.put( (cmd, value) )
+      self.payload = payload
+    self.queue.put( payload )
 
   def peek(self):
-    return self.cmd, self.value
+    return self.payload
 
   def pop(self):
     try:
-      self.cmd, self.value = self.queue.get(False)
+      self.payload = self.queue.get(False)
     except queue.Empty:
       pass
 
@@ -76,6 +95,8 @@ class Comms:
     self.thrMap = {}
     self.onAuth = None
     self.onData = None
+    self.onSetFunction = None
+    self.onSetValue = None
 
   def start(self):
     self.wireless.start()
@@ -97,12 +118,20 @@ class Comms:
 
   def askLocoToAuthorize(self, locoAddr):
     logging.warning(f"Unknown Loco {locoAddr}, ask to authorize")
-    payload = struct.pack('<bf', ord(packetLocoAuth), 0)
+    payload = struct.pack('<BB', ord(CMD_AUTH), 0)
     self.wireless.write(locoAddr, payload)
 
   def send(self, loco, toAddr):
-    cmd, value = loco.peek()
-    payload = struct.pack('<bf', ord(cmd), value)
+    payload = loco.peek()
+    # payload = struct.pack('<BB', ord(cmd), value)
+
+    # if cmd == 'z':
+    #   _key = "keyTest"
+    #   k = len(_key)
+    #   _value = "valueTest"
+    #   v = len(_value)
+    #   payload = struct.pack('<bbbbb{k}s{v}s', ord(cmd), k, v, 0, 0, _key, _value)
+
     if self.wireless.write(toAddr, payload):
       loco.pop()
 
@@ -168,24 +197,44 @@ class Comms:
 
   def onReceive(self, fromNode, payload):
     addr = str(fromNode)
-    packetType = payload[0]
-    if (packetType == ord(packetLocoNorm)):
+    packetType = chr(payload[0])
+
+    logging.error(f">>>:{payload}")
+
+    if (packetType == CMD_HEARTBEAT):
       if addr in self.locoMap:
         loco = self.locoMap[addr]
         self.normalPacket(loco, payload)
       else:
         self.askLocoToAuthorize(fromNode)
-    elif (packetType == ord(packetThrNorm)):
+    elif (packetType == packetThrNorm):
       if addr in self.thrMap:
         thr = self.thrMap[addr]
         self.handleThrottle(thr, payload[1:])
       else:
         self.askThrToAuthorize(fromNode)
-    elif (packetType == ord(packetLocoAuth)):
+    elif (packetType == CMD_AUTH):
       self.authorizeLoco(addr, payload[1:])
-    elif (packetType == ord(packetThrAuth)):
+    elif (packetType == CMD_SET_FUNCTION):
+      if addr in self.locoMap:
+        loco = self.locoMap[addr]
+        unpacked = self.safeUnpack('<BB', payload)
+        functionId = unpacked[1] & 0x7F
+        activate = unpacked[1] & 0x80
+        self.onSetFunction(loco, functionId, activate)
+    elif (packetType == CMD_SET_VALUE):
+      if addr in self.locoMap:
+        loco = self.locoMap[addr]
+        unpacked = struct.unpack('<BB', payload[:2])
+        k = toInt(unpacked[1])
+        v = len(payload) - 2 - k - 2
+        unpacked = struct.unpack(f'<BB{k}sB{v}sB', payload)
+        key = unpacked[2].decode()
+        value = unpacked[4].decode()
+        self.onSetValue(loco, key, value)
+    elif (packetType == packetThrAuth):
       self.askThrToAuthorize(fromNode)
-    elif (packetType == ord(packetThrSub)):
+    elif (packetType == packetThrSub):
       self.authorizeThr(addr, payload[1:])
     else:
       logging.error(f"Unknown packet type: {packetType}")
@@ -205,13 +254,23 @@ class CommsMqtt:
 
   def loop_forever(self):
     self.mqttClient.connect('127.0.0.1')
-    self.mqttClient.subscribe(f'{MQTT_PREFIX_JMRI}/#')
+    options = SubscribeOptions(noLocal=True)
+    self.mqttClient.subscribe(f'{MQTT_PREFIX_JMRI}/#', options=options)
     self.mqttClient.loop_forever()
 
   def onAuth(self, loco):
     nice = ' '.join(loco.fields)
     logging.warning(f"New Loco: v{loco.version} {MQTT_PREFIX_WEB}/{loco.addr}/fileds - {loco.name} {nice}")
     self.mqttClient.publish(f"{MQTT_PREFIX_WEB}/{loco.addr}/fileds", f"{loco.name} {nice}", retain=True)
+
+  def onSetFunction(self, loco, functionId, activate):
+    activate = "ON" if activate else "OFF"
+    logging.warning(f"Function Set: {MQTT_PREFIX_JMRI}/{loco.addr}/function/{functionId} {activate}")
+    self.mqttClient.publish(f"{MQTT_PREFIX_JMRI}/{loco.addr}/function/{functionId}", f"{activate}")
+
+  def onSetValue(self, loco, key, value):
+    logging.warning(f"Value Set: {MQTT_PREFIX_JMRI}/{loco.addr}/value/{key} {value}")
+    self.mqttClient.publish(f"{MQTT_PREFIX_JMRI}/{loco.addr}/value/{key}", f"{value}")
 
   def onData(self, loco):
     nice = [F'{x:.2f}' for x in loco.data]
@@ -228,19 +287,38 @@ class CommsMqtt:
       logging.info(f"MQTT/{locoAddr}/{subTopic}: {payload}")
       if loco:
         if subTopic == 'throttle':
-          loco.push(commandThrottle, payload)
+          loco.push(struct.pack('<BB', ord(CMD_THROTTLE), toInt(payload)))
         elif subTopic == 'direction':
-          if payload == 'FORWARD':
-            loco.push(commandDirection, 1)
-          elif payload == 'REVERSE':
-            loco.push(commandDirection, 2)
-          else:
-            loco.push(commandDirection, 0)
-        elif subTopic == 'function/0':
+          if payload == 'FORWARD' or payload == '1':
+            loco.push(struct.pack('<BB', ord(CMD_DIRECTION), 1))
+          elif payload == 'REVERSE' or payload == '0':
+            loco.push(struct.pack('<BB', ord(CMD_DIRECTION), 0))
+          elif payload == 'STOP' or payload == '2':
+            loco.push(struct.pack('<BB', ord(CMD_DIRECTION), 2))
+          elif payload == 'NEUTRAL' or payload == '3':
+            loco.push(struct.pack('<BB', ord(CMD_DIRECTION), 3))
+        elif subTopic.startswith('function/get'):
+          functionId = toInt(payload)
+          loco.push(struct.pack('<BB', ord(CMD_GET_FUNCTION), functionId))
+        elif subTopic.startswith('function/'):
+          functionId = toInt(subTopic[len('function/'):])
           if payload == 'ON':
-            loco.push(commandLight, 1)
+            loco.push(struct.pack('<BB', ord(CMD_SET_FUNCTION), (1 << 7) | functionId))
           else:
-            loco.push(commandLight, 0)
+            loco.push(struct.pack('<BB', ord(CMD_SET_FUNCTION), functionId))
+        elif subTopic.startswith('value/get'):
+          key = payload.encode()
+          k = len(key)
+          loco.push(struct.pack(f'<BB{k}sB', ord(CMD_GET_VALUE), k, key, 0))
+        elif subTopic.startswith('value/list'):
+          loco.push(struct.pack('<BB', ord(CMD_LIST_VALUE), 0))
+        elif subTopic.startswith('value/'):
+          key = subTopic[len('value/'):].encode()
+          value = payload.encode()
+          k = len(key)
+          v = len(value)
+          packet = struct.pack(f'<BB{k}sB{v}sB', ord(CMD_SET_VALUE), len(key), key, 0, value, 0)
+          loco.push(packet)
         elif subTopic == 'command':
           loco.push(payload[0], payload[1:])
 
@@ -256,6 +334,8 @@ comms = Comms(w)
 cm = CommsMqtt(comms)
 comms.onAuth = cm.onAuth
 comms.onData = cm.onData
+comms.onSetFunction = cm.onSetFunction 
+comms.onSetValue = cm.onSetValue 
 
 comms.start()
 cm.loop_forever()
