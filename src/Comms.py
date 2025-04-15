3befd01deb05127f6db71c3a8b3e5ebe08b9789a
vova <volodymyr.tymoshchuk@gmail.com>
2025-04-14 23:44:27 -0700

    Some porgress on new nRF
    - Push NRF queue to Wireless
    - Clean split between MQ and NRF
    - All handlers are in place
    - Need some testing bugfixing and cleaning

--- src/Comms.py
+++ src/Comms.py
@@ -1,18 +1,18 @@
+import re
 import sys
 import time
 import struct
 import logging
-import queue
 import Wireless
 import paho.mqtt.client as mqtt
 from paho.mqtt.subscribeoptions import SubscribeOptions
-from Config import MQTT_PREFIX_WEB, MQTT_PREFIX_JMRI
+#from Config import MQTT_PREFIX_WEB, MQTT_PREFIX_JMRI
 
 # packetLocoAuth = 'r'
 # packetLocoNorm = 'n'
-packetThrAuth = 'q'
-packetThrSub = 's'
-packetThrNorm = 'p'
+# packetThrAuth = 'q'
+# packetThrSub = 's'
+# packetThrNorm = 'p'
 
 
 # commandThrottle  = 't'
@@ -20,23 +20,12 @@ packetThrNorm = 'p'
 # commandLight     = '@'
 
 
-CMD_AUTH = 'A';
-CMD_HEARTBEAT = 'H';
 
-CMD_THROTTLE = 'T';
-CMD_DIRECTION = 'D';
-CMD_SET_FUNCTION = 'F';
-CMD_GET_FUNCTION = 'P';
-CMD_SET_VALUE = 'S';
-CMD_GET_VALUE = 'G';
-CMD_LIST_VALUE = 'L';
 
-def toInt(value):
-    try:
-        return int(value)
-    except:
-        return 0
 
+
+
+"""
 class Loco:
     def __init__(self, addr, version, fmt, name, fields):
         # self.cmd = 't'
@@ -322,24 +311,283 @@ class CommsMqtt:
                 elif subTopic == 'command':
                     loco.push(payload[0], payload[1:])
 
+"""
 
-logging.basicConfig(level=logging.INFO,
-                                        format='%(asctime)s %(message)s',
-                                        filename='comms.log',
-                                        filemode='a')
+mqMessage = re.compile("cab/(.*?)/(.*)")
+mqPrefix = "cab"
+mqIntro = "intro"
+mqSetThrottle = "throttle";
+mqSetDirection = "direction";
+mqHeartbeatValues = "heartbeat/values"
+mqGetFunction = "function/get"
+mqSetFunction = "function"
+mqGetValue = "value/get"
+mqListValue = "value/list"
+mqSetValue = "value"
+mqKeysValue = "cab/{0}/value/keys"
+
+directions = ["REVERSE", "FORWARD", "STOP", "NEUTRAL"];
+functionON = "ON";
+
+
+CMD_INTRO = 'A';
+CMD_HEARTBEAT = 'H';
+
+CMD_THROTTLE = 'T';
+CMD_DIRECTION = 'D';
+CMD_SET_FUNCTION = 'F';
+CMD_GET_FUNCTION = 'P';
+CMD_SET_VALUE = 'S';
+CMD_GET_VALUE = 'G';
+CMD_LIST_VALUE = 'L';
+
+
+MQTT_NODE_NAME = 'RCC_Station'
+MQTT_BROKER = '127.0.0.1'
+NRF_PINS = (25, 8)
+
+def toInt(value):
+    try:
+        return int(value)
+    except:
+        return 0
+
+
+class TransportNrf:
+    def __init__(self):
+        self.run = True
+        self.known = {}
+        self.subscription = {}
+        self.wireless = Wireless.Wireless(*NRF_PINS)
+        self.wireless.onReceive = self.onReceive
+
+    def write(self, addr, message):
+        logging.debug(f"[NF] >: {addr}: {message}")
+        self.wireless.write(addr, message)
+
+    def writeToSubsribed(self, addr, message):
+        subscribed = self.subscription.get(addr, None)
+        if subscribed:
+            self.write(subscribed, message)
+
+    def askToIntro(self, addr):
+        self.write(addr, struct.pack('<BB', ord(CMD_INTRO), 0))
+
+    def parseIntro(self, message):
+        fields = message.split()
+        return {"Version": fields[0], "Format" : fields[1]}
+
+    def setThrottle(self, addr, value):
+        p = struct.pack('<BB', ord(CMD_THROTTLE), value)
+        self.write(addr, p)
+        self.writeToSubsribed(addr, p)
+
+    def setDirection(self, addr, value):
+        p = struct.pack('<BB', ord(CMD_DIRECTION), value)
+        self.write(addr, p)
+        self.writeToSubsribed(addr, p)
+
+    def getFunction(self, addr, value):
+        p = struct.pack('<BB', ord(CMD_GET_FUNCTION), int(value))
+        self.write(addr, p)
+
+    def setFunction(self, addr, functionId, activate):
+        value = int(functionId) & 0x7F
+        if activate:
+            value |= (1 << 7)
+        p = struct.pack('<BB', ord(CMD_GET_FUNCTION), value)
+        self.write(addr, p)
+
+    def getValue(self, addr, value):
+        key = value.encode()
+        k = len(key)        
+        p = struct.pack(f'<BB{k}sB', ord(CMD_GET_VALUE), k, key, 0)
+        self.write(addr, p)
+    
+    def listValue(self, addr):
+        p = struct.pack('<BB', ord(CMD_LIST_VALUE), 0)
+        self.write(addr, p)
+    
+    def setValue(self, addr, key, value):
+        key = key.encode()
+        value = value.encode()
+        k = len(key)
+        v = len(value)
+        p = struct.pack(f'<BB{k}sB{v}sB', ord(CMD_SET_VALUE), len(key), key, 0, value, 0)
+        self.write(addr, p)
+        
+
+    def start(self):
+        self.wireless.start()
+
+    def onReceive(self, addr, message):
+        if len(message) < 2:
+            return
+
+        logging.debug(f"[NF] <: {addr}/{message}")
+        packetType = chr(message[0])
+
+        if packetType == CMD_INTRO:
+            m = str(message[2:], 'utf-8')
+            self.known[addr] = self.parseIntro(m)
+            mq.processIntro(addr, m)
+        elif addr not in self.known:
+            self.askToIntro(addr)
+        elif packetType == CMD_HEARTBEAT:
+            fmt = '<' + self.known[addr]["Format"];
+            size = struct.calcsize(fmt)
+            if len(message) == size:
+                unpacked = struct.unpack(fmt, message)[1:]
+                nice = ' '.join( [f'{x}' for x in unpacked] ) 
+                mq.processHeartBeat(addr, nice)
+                self.writeToSubsribed(addr, message)
+        elif packetType == CMD_THROTTLE:
+            self.writeToSubsribed(addr, message)
+            mq.setThrottle(addr, message[1])
+        elif packetType == CMD_DIRECTION:
+            self.writeToSubsribed(addr, message)
+            mq.setDirection(addr, message[1])
+        elif packetType == CMD_GET_FUNCTION:
+            self.writeToSubsribed(addr, message)
+            mq.getFunction(addr, message[1])
+        elif packetType == CMD_SET_FUNCTION:
+            self.writeToSubsribed(addr, message)
+            functionId = message[1] & 0x7F
+            activate = message[1] & 0x80
+            mq.setFunction(addr, functionId, activate)
+        elif packetType == CMD_GET_VALUE:
+            self.writeToSubsribed(addr, message)
+            mq.getValue(addr, message[1])
+        elif packetType == CMD_GET_VALUE:
+            self.writeToSubsribed(addr, message)
+            k = len(message) - 3
+            unpacked = struct.unpack(f'<BB{k}sB', message)
+            key = unpacked[2].decode()
+            mq.getValue(addr, key)
+        elif packetType == CMD_SET_VALUE:
+            self.writeToSubsribed(addr, message)
+            unpacked = struct.unpack('<BB', message[:2])
+            k = toInt(unpacked[1])
+            v = len(message) - 2 - k - 2
+            unpacked = struct.unpack(f'<BB{k}sB{v}sB', message)
+            key = unpacked[2].decode()
+            value = unpacked[4].decode()            
+            mq.setValue(addr, key, value)
+        else:
+            logging.error(f"Unknown packet type: {packetType}")
+
+class TransportMqtt:
+    def __init__(self):
+        self.mqttClient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, MQTT_NODE_NAME)
+        self.mqttClient.on_message = self.onReceive
+
+    def start(self):
+        self.mqttClient.connect(MQTT_BROKER)
+        options = SubscribeOptions(noLocal = True)
+        self.mqttClient.subscribe(f'{mqPrefix}/#', options=options)
+        self.mqttClient.loop_forever()
+
+    def write(self, topic, message, retain = False):
+        logging.info(f"[MQ] >: {topic} {message}")
+        self.mqttClient.publish(topic, message, retain)
+
+    def processIntro(self, addr, message):
+        topic = f"{mqPrefix}/{addr}/{mqIntro}"
+        self.write(topic, message, retain = True)
+
+    def processHeartBeat(self, addr, message):
+        topic = f"{mqPrefix}/{addr}/{mqHeartbeatValues}"
+        self.write(topic, message)
+
+    def setThrottle(self, addr, value):
+        topic = f"{mqPrefix}/{addr}/{mqSetThrottle}"
+        self.write(topic, value)
+
+    def setDirection(self, addr, value):
+        topic = f"{mqPrefix}/{addr}/{mqSetDirection}"
+        if value < len(directions):
+            value = directions[value]
+            self.write(topic, value)
+
+    def getFunction(self, addr, functionId):
+        topic = f"{mqPrefix}/{addr}/{mqGetFunction}"
+        self.write(topic, functionId)
+
+    def setFunction(self, addr, functionId, activate):
+        topic = f"{mqPrefix}/{addr}/{mqSetFunction}/{functionId}"
+        activate = "ON" if activate else "OFF"
+        self.write(topic, activate)
+
+    def getValue(self, addr, key):
+        topic = f"{mqPrefix}/{addr}/{mqGetValue}"
+        self.write(topic, key)
+
+    def listValue(self, addr):
+        topic = f"{mqPrefix}/{addr}/{mqListValue}"
+        self.write(topic, "")
+    
+    def setValue(self, addr, key, value):
+        topic = f"{mqPrefix}/{addr}/{mqSetValue}/{key}"
+        self.write(topic, value)
+
+
+    def onReceive(self, client, userdata, msg):
+        topic = mqMessage.match(msg.topic)
+        if (topic is None):
+            return
+
+        message = str(msg.payload, 'UTF-8')
+        logging.debug(f"[MQ] <: {msg.topic} {message}")
+        addr, action = topic.groups()
+
+        if action == mqSetThrottle:
+            nrf.setThrottle(addr, toInt(message))
+        elif action == mqSetDirection:
+            for i, s in enumerate(directions):
+                if message == s or message == str(i):
+                    nrf.setDirection(addr, i)
+                    break
+        elif action == mqGetFunction:
+            nrf.getFunction(addr, message)
+        elif action.startswith(mqSetFunction):
+            nrf.setFunction(addr, action.split('/')[1], message == "ON")
+        elif action == mqGetValue:
+            nrf.getValue(addr, message)
+        elif action == mqListValue:
+            nrf.listValue(addr)
+        elif action.startswith(mqSetValue):
+            nrf.setValue(addr, action.split('/')[1], message)
+
+
+
+
+
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    filename='comms.log',
+                    filemode='a')
 logging.error('Start')
 
-w = Wireless.Wireless(25, 8)
-comms = Comms(w)
-cm = CommsMqtt(comms)
-comms.onAuth = cm.onAuth
-comms.onData = cm.onData
-comms.onSetFunction = cm.onSetFunction 
-comms.onSetValue = cm.onSetValue 
+nrf = TransportNrf()
+mq = TransportMqtt()
 
-comms.start()
-cm.loop_forever()
+nrf.start()
+mq.start()
 
-comms.stop()
-logging.error('Stop')
+
+# w = Wireless.Wireless(25, 8)
+# comms = Comms(w)
+# cm = CommsMqtt(comms)
+# comms.onAuth = cm.onAuth
+# comms.onData = cm.onData
+# comms.onSetFunction = cm.onSetFunction 
+# comms.onSetValue = cm.onSetValue 
+
+# comms.start()
+# cm.loop_forever()
+
+# comms.stop()
+# logging.error('Stop')
 
