301979959e0a9e59a9022fe9a7c3f7924308b737
vova <volodymyr.tymoshchuk@gmail.com>
2025-05-02 22:13:01 -0700

    Step 4

--- src/testQ.py
+++ src/testQ.py
@@ -1,11 +1,21 @@
-import re, struct
+import re
+import struct
+import logging
+import Wireless
+import paho.mqtt.client as mqtt
+from paho.mqtt.subscribeoptions import SubscribeOptions
+
+MQTT_BROKER = '127.0.0.1'
+MQTT_NODE_NAME = 'RCC_Station'
+NRF_PINS = (25, 8)
 
 MQ_MESSAGE = re.compile("cab/(.*?)/(.*)")
+MQ_PREFIX = "cab"
 MQ_DIRECTIONS = ["REVERSE", "FORWARD", "STOP", "NEUTRAL"]
-
 NRF_SEPARATOR = ' '
 NRF_TYPE_LOCO = 'L'
 NRF_TYPE_KEYPAD = 'K'
+NRF_INTRO = 'A'
 NRF_SUB = 'B'
 NRF_LIST_CAB = 'C'
 
@@ -27,22 +37,13 @@ def directionToMq(x):
         return '', MQ_DIRECTIONS[i]
     return '', str(x)
 
-def introToNrf(k, v):
-    t.processIntro(v)
-    return bytes(v, 'utf-8')
-
-def introToMq(x):
-    x = x.decode()
-    t.processIntro(x)
-    return '', x
-
 def heartbeatToNrf(k, v):
-    fmt = '<' + t.getHeartbeatFmt()
+    fmt = '<' + broker.getHeartbeatFmt()
     unpacked = [int(i) for i in v.split()]
     return struct.pack(fmt, *unpacked)
 
 def heartbeatToMq(v):
-    fmt = '<' + t.getHeartbeatFmt()
+    fmt = '<' + broker.getHeartbeatFmt()
     size = struct.calcsize(fmt)
     if len(v) == size:
         unpacked = struct.unpack(fmt, v)
@@ -60,12 +61,12 @@ class RouteEntry:
             return re.compile(value)
 
 PROTO_MAP = [ \
-    RouteEntry('A',  introToNrf,        'intro',             introToMq         ),
+    RouteEntry('A',  '{value}',         'intro',             '{value}'         ),
     RouteEntry('H',  heartbeatToNrf,    'heartbeat/values',  heartbeatToMq     ),
     RouteEntry('J',  '{value}',         'keys',              '{value}'         ),
     RouteEntry('T',  '{intvalue}',      'throttle',          '{intvalue}',     ),
     RouteEntry('D',  directionToNrf,    'direction',         directionToMq     ),
-    RouteEntry('P',  '{intvalue}',      'function/get',      '{intvalue}'         ),
+    RouteEntry('P',  '{intvalue}',      'function/get',      '{intvalue}'      ),
     RouteEntry('F',  functionToNrf,     'function/{key}',    functionToMq,     ),
     RouteEntry('L',  lambda x,y: 0,     'value/list',        lambda x: ('','') ),
     RouteEntry('J',  '{value}',         'keys',              '{value}'         ),
@@ -77,8 +78,11 @@ PROTO_MAP = [ \
 
 
 class Translator:
-    def __init__(self,):
-        self.addr = 0
+    def toInt(self, x):
+        try:
+            return abs(int(x))
+        except:
+            return 0
 
     def getKey(self, keyMatch):
         if keyMatch:
@@ -98,7 +102,7 @@ class Translator:
         if callable(output):
             return bytes(entry.nrf[0], 'utf-8') + bytes(output(key, value))
         elif output == '{intvalue}':
-            return bytes(entry.nrf[0], 'utf-8') + bytes([int(value)])
+            return bytes(entry.nrf[0], 'utf-8') + bytes([self.toInt(value)])
         else:
             output = output.replace('{key}', key).replace('{value}', value)
             return bytes(entry.nrf[0], 'utf-8') + bytes(output, 'utf-8')
@@ -116,7 +120,7 @@ class Translator:
             return entry.mq[0].replace('{key}', key), entry.mq[1].replace('{value}', value)
         return None
 
-    def toNrf(self, addr, action, message):
+    def toNrf(self, action, message):
         for entry in PROTO_MAP:
             a = entry.mq[2].match(action)
             if a:
@@ -126,88 +130,143 @@ class Translator:
                     return nrf
 
     def toMq(self, action, message):
-        if action == NRF_SUB:
-            t.processSub(message)
-        elif action == NRF_LIST_CAB:
-            t.processListCab(message)
-        else:
-            for entry in PROTO_MAP:
-                if ord(entry.nrf[0]) == action:
-                    nrf = self.buildMq(entry, message)
-                    if nrf:
-                        return nrf
+        for entry in PROTO_MAP:
+            if ord(entry.nrf[0]) == action:
+                nrf = self.buildMq(entry, message)
+                if nrf:
+                    return nrf
 
 
+class Broker:
 
+    def __init__(self):
+        self.known = {}
+        self.subscription = {}
 
-    def processIntro(self, message):
-        pass
-        # print(">>>>>>>1>", message.split(' '))
-        # fields = message.split()
-        # m = { "Type": fields[0], "Addr": fields[1], "Name": fields[2], "Version": fields[3] }
-        # if len(fields) > 4:
-        #     m["Format"] = fields[4]
-        # self.known[addr] = m
+    def askToIntro(self, addr):
+        nrf.write(addr, struct.pack('<BB', ord(NRF_INTRO), 0))
 
-    def processSub(self, message):
-        print(">>>>>>>2>", message)
+    def processIntro(self, addr, message):
+        fields = message.split()
+        m = { "Type": fields[0], "Addr": fields[1], "Name": fields[2], "Version": fields[3] }
+        if len(fields) > 4:
+            m["Format"] = fields[4]
+        self.known[addr] = m
+
+    def processSub(self, addr, subTo):
+        addr = int(addr)
+        subTo = int(subTo)
+        self.subscription[addr] = subTo
+        self.subscription[subTo] = addr
 
     def processListCab(self, message):
-        print(">>>>>>>3>", message)
+        p = NRF_SEPARATOR.join( [f'{fields["Type"]} {fields["Addr"]} {fields["Name"]}' for addr, fields in self.known.items()] )
+        p = struct.pack(f'<B{len(p)}s', ord(NRF_LIST_CAB), p.encode())
+        nrf.write(addr, p)
 
     def getHeartbeatFmt(self):
-        # return self.known[addr]["Format"]
-        return 'BB'
+        return self.known[self.addr]["Format"]
+
+    def getForwardNrf(self, addr):
+        addr = int(addr)
+        return self.subscription.get(addr, 0)
+
+    def getForwardMq(self, addr):
+        k = self.known.get(addr, 0)
+        sub = self.getForwardNrf(addr)
+        if k:
+            return sub if k["Type"] == NRF_TYPE_KEYPAD else addr
+
+    def receiveNrf(self, addr, action, message):
+        if addr not in self.known and action != NRF_INTRO:
+            self.askToIntro(addr)
+            return
+        if action == NRF_SUB:
+            self.processSub(addr, message[1])
+            return
+        if action == NRF_LIST_CAB:
+            self.processListCab(message)
+            return
+
+        if action == NRF_INTRO:
+            self.processIntro(addr, message)
+        self.addr = addr
+        fwdPacket = translator.toMq(action, message)
+        fwdMqAddr = self.getForwardMq(addr)
+        mq.write(fwdMqAddr, fwdPacket)
+        fwdNrfAddr = self.getForwardNrf(addr)
+        if fwdNrfAddr:
+            nrf.write(fwdNrfAddr, message)
+
+    def receiveMq(self, addr, action, message):
+        self.addr = addr
+        fwdPacket = translator.toNrf(action, message)
+        fwdNrfAddr = self.getForwardNrf(addr)
+        if fwdNrfAddr:
+            nrf.write(fwdNrfAddr, fwdPacket)
 
 
-t = Translator()
+
+class TransportNrf:
+    def __init__(self):
+        self.wireless = Wireless.Wireless(*NRF_PINS)
+        self.wireless.onReceive = self.onReceive
+
+    def start(self):
+        self.wireless.start()
+
+    def write(self, addr, message):
+        addr = int(addr)
+        logging.debug(f"[NF] >: {addr}/{message}")
+        self.wireless.write(addr, message)
+
+    def onReceive(self, addr, packet):
+        if len(message) < 2:
+            return
+        logging.debug(f"[NF] <: {addr}/{message}")
+        broker.receiveNrf(addr, chr(packet[0]), packet[1:])
 
 
+class TransportMqtt:
+    def __init__(self):
+        self.mqttClient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, MQTT_NODE_NAME)
+        self.mqttClient.on_message = self.onReceive
+        self.cache = ''
+
+    def start(self):
+        self.mqttClient.connect(MQTT_BROKER)
+        options = SubscribeOptions(qos = 1, noLocal = True)
+        self.mqttClient.subscribe(f'{MQ_PREFIX}/#', options = options)
+        self.mqttClient.loop_forever()
+
+    def write(self, topic, message, retain = False):
+        logging.info(f"[MQ] >: {topic} {message}")
+        self.cache = f"{topic} {message}"
+        self.mqttClient.publish(topic, message, retain)
+
+    def onReceive(self, client, userdata, msg):
+        topic = MQ_MESSAGE.match(msg.topic)
+        if (topic is None):
+            return
+        message = str(msg.payload, 'UTF-8')
+        cache = f"{msg.topic} {message}"
+        if cache == self.cache:
+            return
+        logging.debug(f"[MQ] <: {msg.topic} {message}")
+        addr, action = topic.groups()
+        broker.receiveMq(addr, action, message)
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    filename='comms.log',
+                    filemode='a')
+logging.error('Start')
 
+translator = Translator()
+broker = Broker()
+nrf = TransportNrf()
+mq = TransportMqtt()
 
-testProtocolData = [
-('cab/3/throttle+99',           b'Tc'),
-('cab/3/function/get+2',        b'P\x02'),
-('cab/3/function/3+ON',         b'F\x83'),
-('cab/3/function/3+OFF',        b'F\x03'),
-('cab/3/value/get+zupa',        b'Gzupa'),
-('cab/3/value/zupa+abc',        b'Szupa abc'),
-('cab/3/value/list+',           b'L'),
-('cab/3/direction+FORWARD',     b'D\x01'),
-('cab/3/direction+REVERSE',     b'D\x00'),
-('cab/3/direction+NEUTRAL',     b'D\x03'),
-('cab/3/intro+L 3 Rcc 0.9 B',   b'AL 3 Rcc 0.9 B'),
-('cab/3/heartbeat/values+1 2',  b'H\x01\x02'),
-]
-
-
-
-def testToNrf(incoming):
-    topic, message = incoming.split('+')
-    topic = MQ_MESSAGE.match(topic)
-    addr, action = topic.groups()
-    return t.toNrf(addr, action, message)
-
-def testToMq(incoming):
-    action, message = (incoming[0], incoming[1:])
-    topic, msg = t.toMq(action, message)
-    return 'cab/3/' + topic + '+' + msg
-
-def testResult(incoming, outgoing, expected):
-    line = f'{incoming}'.ljust(28)
-    line += f' ->   {outgoing}'.ljust(38)
-    print(line, end='')
-    if outgoing == expected:
-        print('ok')
-    else:
-        print('FAIL')
-
-for mq, nrf in testProtocolData:
-    nrfAct = testToNrf(mq)
-    testResult(mq, nrfAct, nrf)
-
-for mq, nrf in testProtocolData:
-    mqAct = testToMq(nrf)
-    testResult(nrf, mqAct, mq)
+nrf.start()
+mq.start()
